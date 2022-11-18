1eb3611ddf06e1edc24fb3ee6d806da8b6c6f81c
Ivan Tikhonov <itikhonov@mail.com>
2022-11-18 13:07:31 +0300

    base32 refactoring

--- IT.Id/Id.Base32.cs
+++ IT.Id/Id.Base32.cs
@@ -9,49 +9,40 @@ public readonly partial struct Id
         unsafe
         {
             fixed (char* resultP = result)
-            fixed (char* pAlphabet = Base32.ALPHABET)
+            fixed (char* map = Base32.ALPHABET)
             {
                 ulong value = ((ulong)_timestamp << 8) | (byte)(_b >> 24);
 
-                resultP[0] = pAlphabet[value >> 35];
-                resultP[1] = pAlphabet[(value >> 30) & 0x1F];
-                resultP[2] = pAlphabet[(value >> 25) & 0x1F];
-                resultP[3] = pAlphabet[(value >> 20) & 0x1F];
-                resultP[4] = pAlphabet[(value >> 15) & 0x1F];
-                resultP[5] = pAlphabet[(value >> 10) & 0x1F];
-                resultP[6] = pAlphabet[(value >> 5) & 0x1F];
-                resultP[7] = pAlphabet[value & 0x1F];
+                resultP[0] = map[value >> 35];
+                resultP[1] = map[(value >> 30) & 0x1F];
+                resultP[2] = map[(value >> 25) & 0x1F];
+                resultP[3] = map[(value >> 20) & 0x1F];
+                resultP[4] = map[(value >> 15) & 0x1F];
+                resultP[5] = map[(value >> 10) & 0x1F];
+                resultP[6] = map[(value >> 5) & 0x1F];
+                resultP[7] = map[value & 0x1F];
 
-                var byte5 = (byte)(_b >> 16);
-                var byte6 = (byte)(_b >> 8);
-                var byte7 = (byte)(_b);
-                var byte8 = (byte)(_c >> 24);
-                var byte9 = (byte)(_c >> 16);
+                value = (byte)(_b >> 16);
+                value = (value << 8) | (byte)(_b >> 8);
+                value = (value << 8) | (byte)_b;
+                value = (value << 8) | (byte)(_c >> 24);
+                value = (value << 8) | (byte)(_c >> 16);
 
-                value = byte5;
-                value = (value << 8) | byte6;
-                value = (value << 8) | byte7;
-                value = (value << 8) | byte8;
-                value = (value << 8) | byte9;
+                resultP[8] = map[value >> 35];
+                resultP[9] = map[(value >> 30) & 0x1F];
+                resultP[10] = map[(value >> 25) & 0x1F];
+                resultP[11] = map[(value >> 20) & 0x1F];
+                resultP[12] = map[(value >> 15) & 0x1F];
+                resultP[13] = map[(value >> 10) & 0x1F];
+                resultP[14] = map[(value >> 5) & 0x1F];
+                resultP[15] = map[value & 0x1F];
 
-                var byte10 = (byte)(_c >> 8);
-                var byte11 = (byte)(_c);
+                value = (((ulong)(byte)(_c >> 8) << 8) | (byte)_c) << 4;
 
-                resultP[8] = pAlphabet[value >> 35];
-                resultP[9] = pAlphabet[(value >> 30) & 0x1F];
-                resultP[10] = pAlphabet[(value >> 25) & 0x1F];
-                resultP[11] = pAlphabet[(value >> 20) & 0x1F];
-                resultP[12] = pAlphabet[(value >> 15) & 0x1F];
-                resultP[13] = pAlphabet[(value >> 10) & 0x1F];
-                resultP[14] = pAlphabet[(value >> 5) & 0x1F];
-                resultP[15] = pAlphabet[value & 0x1F];
-
-                value = (((ulong)byte10 << 8) | byte11) << 4;
-
-                resultP[16] = pAlphabet[value >> 15];
-                resultP[17] = pAlphabet[(value >> 10) & 0x1F];
-                resultP[18] = pAlphabet[(value >> 5) & 0x1F];
-                resultP[19] = pAlphabet[value & 0x1F];
+                resultP[16] = map[value >> 15];
+                resultP[17] = map[(value >> 10) & 0x1F];
+                resultP[18] = map[(value >> 5) & 0x1F];
+                resultP[19] = map[value & 0x1F];
             }
         }
 
@@ -74,20 +65,11 @@ public readonly partial struct Id
             resultP[6] = map[(value >> 5) & 0x1F];
             resultP[7] = map[value & 0x1F];
 
-            var byte5 = (byte)(_b >> 16);
-            var byte6 = (byte)(_b >> 8);
-            var byte7 = (byte)(_b);
-            var byte8 = (byte)(_c >> 24);
-            var byte9 = (byte)(_c >> 16);
-
-            value = byte5;
-            value = (value << 8) | byte6;
-            value = (value << 8) | byte7;
-            value = (value << 8) | byte8;
-            value = (value << 8) | byte9;
-
-            var byte10 = (byte)(_c >> 8);
-            var byte11 = (byte)(_c);
+            value = (byte)(_b >> 16);
+            value = (value << 8) | (byte)(_b >> 8);
+            value = (value << 8) | (byte)_b;
+            value = (value << 8) | (byte)(_c >> 24);
+            value = (value << 8) | (byte)(_c >> 16);
 
             resultP[8] = map[value >> 35];
             resultP[9] = map[(value >> 30) & 0x1F];
@@ -98,7 +80,7 @@ public readonly partial struct Id
             resultP[14] = map[(value >> 5) & 0x1F];
             resultP[15] = map[value & 0x1F];
 
-            value = (((ulong)byte10 << 8) | byte11) << 4;
+            value = (((ulong)(byte)(_c >> 8) << 8) | (byte)_c) << 4;
 
             resultP[16] = map[value >> 15];
             resultP[17] = map[(value >> 10) & 0x1F];
@@ -123,20 +105,11 @@ public readonly partial struct Id
             resultP[6] = map[(value >> 5) & 0x1F];
             resultP[7] = map[value & 0x1F];
 
-            var byte5 = (byte)(_b >> 16);
-            var byte6 = (byte)(_b >> 8);
-            var byte7 = (byte)(_b);
-            var byte8 = (byte)(_c >> 24);
-            var byte9 = (byte)(_c >> 16);
-
-            value = byte5;
-            value = (value << 8) | byte6;
-            value = (value << 8) | byte7;
-            value = (value << 8) | byte8;
-            value = (value << 8) | byte9;
-
-            var byte10 = (byte)(_c >> 8);
-            var byte11 = (byte)(_c);
+            value = (byte)(_b >> 16);
+            value = (value << 8) | (byte)(_b >> 8);
+            value = (value << 8) | (byte)_b;
+            value = (value << 8) | (byte)(_c >> 24);
+            value = (value << 8) | (byte)(_c >> 16);
 
             resultP[8] = map[value >> 35];
             resultP[9] = map[(value >> 30) & 0x1F];
@@ -147,7 +120,7 @@ public readonly partial struct Id
             resultP[14] = map[(value >> 5) & 0x1F];
             resultP[15] = map[value & 0x1F];
 
-            value = (((ulong)byte10 << 8) | byte11) << 4;
+            value = (((ulong)(byte)(_c >> 8) << 8) | (byte)_c) << 4;
 
             resultP[16] = map[value >> 15];
             resultP[17] = map[(value >> 10) & 0x1F];
