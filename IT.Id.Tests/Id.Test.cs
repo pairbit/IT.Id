fe7d6c787efe84c85b2debaac6f30aa4ec9d7ca1
Ivan Tikhonov <itikhonov@mail.com>
2022-11-29 15:42:04 +0300

    fix warning test

--- IT.Id.Tests/Id.Test.cs
+++ IT.Id.Tests/Id.Test.cs
@@ -18,21 +18,25 @@ public class IdTest
     {
         var id = Id.New();
         var objectId = Id.NewObjectId();
+        Assert.Multiple(() =>
+        {
+            Assert.That(id.MachineName, Is.Null);
+            Assert.That(objectId.MachineName, Is.Null);
+            Assert.That(id.Machine, Is.EqualTo(Id.MachineHash24));
+            Assert.That(objectId.Machine, Is.Not.EqualTo(Id.MachineHash24));
+        });
+        string[] names = null!;
 
-        Assert.IsNull(id.MachineName);
-        Assert.IsNull(objectId.MachineName);
-
-        Assert.That(id.Machine, Is.EqualTo(Id.MachineHash24));
-        Assert.That(objectId.Machine, Is.Not.EqualTo(Id.MachineHash24));
-
-        Assert.Throws<ArgumentNullException>(() => Id.Machines(null));
+        Assert.Throws<ArgumentNullException>(() => Id.Machines(names));
         Assert.Throws<ArgumentException>(() => Id.Machines());
         Assert.Throws<ArgumentException>(() => Id.Machines("abc", "abc"));
 
         Id.Machines(Environment.MachineName, "IT2");
         Assert.Throws<InvalidOperationException>(() => Id.Machines("IT3"));
-
-        Assert.That(id.MachineName, Is.EqualTo(Environment.MachineName));
-        Assert.IsNull(objectId.MachineName);
+        Assert.Multiple(() =>
+        {
+            Assert.That(id.MachineName, Is.EqualTo(Environment.MachineName));
+            Assert.That(objectId.MachineName, Is.Null);
+        });
     }
 }
